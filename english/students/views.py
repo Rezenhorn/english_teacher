96d441fd82f049e7e4316d2c6ac48f95c95d99db
Dmitry Fomichev <torman37rus@gmail.com>
2023-07-23 18:48:15 +0300

    style: fix flake8

--- english/students/views.py
+++ english/students/views.py
@@ -65,7 +65,7 @@ class HomeworkUpdateView(LoginRequiredMixin,
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
-        student = get_object_or_404(User, username=self.kwargs.get('username'))
+        student = get_object_or_404(User, username=self.kwargs.get("username"))
         data.update(title=f"Edit homework for {student}")
         return data
 

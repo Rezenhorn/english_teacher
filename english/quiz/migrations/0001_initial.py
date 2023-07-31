da7ce96057b1b81c2239f45a6737078944055082
Dmitry Fomichev <torman37rus@gmail.com>
2023-07-31 19:52:59 +0300

    fix: fix migration

--- english/quiz/migrations/0001_initial.py
+++ english/quiz/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('dictionary', '0002_copy_old_data'),
+        ('dictionary', '0004_auto_20230731_1750'),
     ]
 
     operations = [
